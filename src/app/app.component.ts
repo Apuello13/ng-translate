eab1d8fdd4a6a6b5880c36c86d56a270cdd8d197
Andres Puello <andrespuello53@gmail.com>
2022-11-05 17:08:43 -0500

    add desing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'translate';
+export class AppComponent implements OnInit {
+  isHiddenSplash: boolean = false;
+  text: string = '';
+  resultText: string = 'Here you look the result';
+
+  ngOnInit(): void {
+    this.hiddenSplashScreen();
+  }
+
+  hiddenSplashScreen(): void {
+    setTimeout(() => (this.isHiddenSplash = !this.isHiddenSplash), 2000);
+  }
 }
